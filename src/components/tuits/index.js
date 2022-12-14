580d5927cee18a9bfc062045e0303854019333c6
Alamu Ramasamy <54072236+alamuramasamy9@users.noreply.github.com>
2022-12-14 15:17:22 -0500

    explore search

--- src/components/tuits/index.js
+++ src/components/tuits/index.js
@@ -6,20 +6,31 @@ import * as tuitService from "../../services/tuits-service";
 import * as bookmarkService from "../../services/bookmark-service";
 
 const Tuits = ({tuits = [], refreshTuits}) => {
-    const likeTuit = (tuit) =>
-        likesService.userLikesTuit('me', tuit._id)
+    const likeTuit = (tuit) => {
+        const user = JSON.parse(localStorage.getItem("user"));
+
+        likesService.userLikesTuit(user._id, tuit._id)
+            .then(refreshTuits)
+            .catch(e => alert(e))
+    }
+
+    const dislikeTuit = (tuit) => {
+        const user = JSON.parse(localStorage.getItem("user"));
+
+        likesService.userDislikesTuit(user._id, tuit._id)
             .then(refreshTuits)
             .catch(e => alert(e))
 
-    const dislikeTuit = (tuit) =>
-        likesService.userDislikesTuit('me', tuit._id)
-            .then(refreshTuits)
-            .catch(e => alert(e))
-    
-    const bookmarkToggle = (tuit) =>
-        bookmarkService.userBookmarksTuit('me', tuit._id)
+    }
+
+    const bookmarkToggle = (tuit) => {
+        const user = JSON.parse(localStorage.getItem("user"));
+
+        bookmarkService.userBookmarksTuit(user._id, tuit._id)
             .then(refreshTuits)
             .catch(e =>alert(e))
+    }
+
 
     const deleteTuit = (tid) =>
         tuitService.deleteTuit(tid)
